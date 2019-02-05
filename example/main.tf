5042a35ea4b8c9458aaf090ecc7baa44fcf22de8
David Povey <david.povey@digital.cabinet-office.gov.uk>
2019-02-05 15:12:47 +0000

    Minor fix to work with multiple EC2 instances
    Solo: @poveyd

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ module "ec2_restart_example" {
   source = "git::git@github.com:alphagov/terraform-aws-ec2-restart.git"
 
   resource_prefix = "test"
-  ec2_ids         = ["${aws_instance.example.id}"]
+  ec2_ids         = ["${aws_instance.example.*.id}"]
   debug           = 0
 
   custom_tags = {
